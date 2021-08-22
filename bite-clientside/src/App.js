505af3b5045cce6361479a3c8c5d1979ac62261c
Marsha Hall <mphall77@gmail.com>
2021-08-22 10:25:49 -0400

    delete dead code in app.js

--- bite-clientside/src/App.js
+++ bite-clientside/src/App.js
@@ -8,46 +8,44 @@ import Recipes from "./Components/Recipes";
 import "./App.css";
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
-  const [category, setCategory] = useState("");
+	const [recipes, setRecipes] = useState([]);
+	const [category, setCategory] = useState("");
 
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        console.log(process.env.REACT_APP_API_KEY)
-        debugger
-        const res = await axios.get(
-          `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&diet=${category}&number=10`
-        );
-        setRecipes(res.data.results);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchRecipes();
-  }, [category]);
+	useEffect(() => {
+		const fetchRecipes = async () => {
+			try {
+				const res = await axios.get(
+					`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&diet=${category}&number=10`
+				);
+				setRecipes(res.data.results);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		fetchRecipes();
+	}, [category]);
 
-  return (
-    <main className="App">
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <Home setCategory={setCategory} />
-          </Route>
-          {/* Index */}
-          <Route exact path="/recipes">
-			 {/* mapping through the array */}
-            <Recipes recipes={recipes} category={category}/>
-          </Route>
-          {/* Show */}
-          <Route exact path="/recipes/:id">
-            <Recipe />
-          </Route>
-        </Switch>
-      </Router>
-    </main>
-  );
+	return (
+		<main className="App">
+			<Router>
+				<NavBar />
+				<Switch>
+					<Route exact path="/">
+						<Home setCategory={setCategory} />
+					</Route>
+					{/* Index */}
+					<Route exact path="/recipes">
+						{/* mapping through the array */}
+						<Recipes recipes={recipes} category={category} />
+					</Route>
+					{/* Show */}
+					<Route exact path="/recipes/:id">
+						<Recipe />
+					</Route>
+				</Switch>
+			</Router>
+		</main>
+	);
 }
 
 export default App;
