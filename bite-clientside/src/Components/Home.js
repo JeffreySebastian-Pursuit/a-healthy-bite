3e553cd1b0ae05dde54635bd5acdb3dc28f03cef
Jeffrey <jeffreysebastian@pursuit.org>
2021-08-22 18:15:47 -0400

    Redeployment

--- bite-clientside/src/Components/Home.js
+++ bite-clientside/src/Components/Home.js
@@ -1,8 +1,4 @@
 import { useHistory } from "react-router-dom";
-<<<<<<< HEAD
-
-=======
->>>>>>> ebf96ac705f78a60cb10b11bb16638db5a1930f3
 const Home = ({ setCategory }) => {
   const history = useHistory();
   const handleSelection = () => {
